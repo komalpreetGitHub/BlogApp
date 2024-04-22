6a5559c40e0176881e0663b20e775be20417cdc4
komalpreet <kaurkomal0810@gmail.com>
2024-04-22 13:25:16 +0530

    newchanges

---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./nav";
+
+
+export default function Home() {
+    return(<div>
+        <Navbar/>
+        <br></br>
+    <center><h1>Home Page</h1></center>
+    </div> 
+    );
+}
---
+++ frontend/src/components/nav.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
+import { useState } from 'react';
+import { IoMdMenu } from "react-icons/io";
+import { FaTimes } from "react-icons/fa";
+
+export default function Navbar () {
+
+    const[isMobile,setIsMobile] = useState(false);
+
+    return(
+        <nav className='navbar'>
+             <h3 className='logo'>BLOG</h3>
+             <ul className={isMobile? "nav-links-mobile" : "nav-links"}
+             onClick={() => setIsMobile(false)}>
+                 <Link to="" className='blog'>
+                    <li><button className='form_btn'>Add Blog</button></li>
+                   </Link>
+                   <Link to="/" className='home'>
+                    <li>Home</li>
+                   </Link>
+                
+                   {/* <Link to="/login" className='login'>
+                    <li>Login</li>
+                   </Link>
+                   <Link to="/signup" className='signup'>
+                    <li>Signup</li>
+                   </Link> */}
+                   <Link to="" className='logout'>
+                    <li>Logout</li>
+                   </Link>
+             </ul>
+             <button className='mobile-menu-icon'
+             onClick={() => setIsMobile(!isMobile)}>
+                    {isMobile? 
+                    (<FaTimes />):
+                    (<IoMdMenu />)
+                    }
+             </button>
+        </nav>
+    ); 
+}
+
+ 
